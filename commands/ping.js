9d573eb1557f5241fa85bc60366b5c5b0f64ca01
C. Marcus DiMarco <79232853+cmarcusdimarco@users.noreply.github.com>
2023-01-10 12:54:11 -0500

    Changed ping message.

--- commands/ping.js
+++ commands/ping.js
@@ -4,8 +4,8 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
-        .setDescription('Replies with Keep yourself safe.'),
+        .setDescription('Replies with Pong.'),
     async execute(interaction) {
-        await interaction.reply('Keep yourself safe.');
+        await interaction.reply('Pong.');
     },
 };
